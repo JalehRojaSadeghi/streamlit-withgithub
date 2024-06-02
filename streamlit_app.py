87ecd1df48c149bc5b918fbbb8dcf11091e677a0
JalehRojaSadeghi <115509016+JalehRojaSadeghi@users.noreply.github.com>
2024-06-02 12:34:57 +1200

    ABM_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,22 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import streamlit.components.v1 as components
 import streamlit as st
+from PIL import Image
 
-"""
-# Welcome to Streamlit!
+# Load the HTML file in Streamlit
+st.sidebar.title('Choose your favorite output')
+option=st.sidebar.selectbox('select graph',('network','at risk projects', 'graph'))
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if option=='network':
+    
+    HtmlFile = open('ABM.html', 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    components.html(source_code, height=500)
+    
+if option == 'at risk projects':
+    image = Image.open('ABM_atriskprojects.jpg')
+    st.image(image, use_column_width=True)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if option == 'graph':
+    image = Image.open('ABM_graph.jpg')
+    st.image(image, use_column_width=True)
